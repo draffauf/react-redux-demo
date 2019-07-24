9008489e5cdd32e857c0901478f1b9197ed328ee
David Raffauf <services@davidraffauf.com>
2019-07-24 09:43:53 -0700

    Set up crude store, state, actions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
+import { createStore } from 'redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
+  console.log('reducer', state, action);
+
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
 }
+
+const store = createStore(reducer);
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "RESET" });
+
+// const initialState = {
+//   count: 0
+// };
+
+// function reducer(state = initialState, action) {
+//   console.log('reducer', state, action);
+//   return state;
+// }
+
+// const store = createStore();
+
+const App = () => (
+  <div>
+    <Counter/>
+  </div>
+);
 
 export default App;
